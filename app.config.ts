b1bcef7408577f709474d28d4d8229c183499c1a
Joey Enrique Soliman <solimanjoseenrique@gmail.com>
2025-12-04 18:48:29 +0800

    fixing supabase keys

--- app.config.ts
+++ app.config.ts
@@ -17,38 +17,44 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
   },
-android: {
-  adaptiveIcon: {
-    foregroundImage: './assets/adaptive-icon-v2.png',
-    backgroundColor: '#ffffff',
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon-v2.png',
+      backgroundColor: '#ffffff',
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+    package: 'com.mrjoey0125.mobileappv3',
+    jsEngine: 'hermes',
   },
-  edgeToEdgeEnabled: true,
-  predictiveBackGestureEnabled: false,
-  package: 'com.mrjoey0125.mobileappv3',
-  jsEngine: 'hermes',
-},
   web: {
     favicon: './assets/favicon.png',
   },
-  plugins: [
-    'expo-web-browser',
-    'expo-build-properties',
-  ],
+plugins: [
+  "expo-build-properties",
+  "expo-web-browser",
+  "expo-image-picker",
+  "expo-file-system",
+  "expo-system-ui",
+  "expo-splash-screen",
+  "expo-updates"
+],
   extra: {
-    SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL!,
-    SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!,
-    firebaseApiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY!,
-    firebaseAuthDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-    firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID!,
-    firebaseStorageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-    firebaseMessagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-    firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID!,
-    firebaseMeasurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID!,
-    googleClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID!,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    firebaseApiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    firebaseAuthDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    firebaseStorageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    firebaseMessagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    firebaseMeasurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    googleClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
     eas: {
-      projectId: process.env.EXPO_PUBLIC_EAS_PROJECT_ID!,
+      projectId: process.env.EXPO_PUBLIC_EAS_PROJECT_ID,
     },
   },
 };
 
-export default ({ config: _ }: { config: ExpoConfig }) => config;
+
+export default config;
